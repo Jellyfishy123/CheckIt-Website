ae02a692d3ec1ace822e40008c8a556c84af3313
Charmain <122718988+Charkaka@users.noreply.github.com>
2024-03-26 23:15:44 +0800

    Update main with todo  (#4)
    * Update button styles in index.css
    
    * Update button colors in index.css and change high priority color in index.js
    
    * Add clear button and configured checkbox
    
    * Checkboxes, CLEAR button, paddings
    
    * Add Firebase configuration and tasks service
    
    * Remove frontend/vendors/README.md file
    
    * Move .gitignore to root
    
    * Refactor task form and view functions
    
    * fix(task): fix indoor/outdoor radio button
    
    * Add due task detection and due task display functionality
    
    * Refactor task detection and add weather container
    
    * Update task detection container styling and height
    
    * Add weatherConfig and weatherService files
    
    * Add weatherConfig.js to .gitignore
    
    * Remove weatherAPIConfig file from git
    
    * Add locationConfig.js and update task.js

--- frontend/resources/js/tasks.js
+++ frontend/resources/js/tasks.js
@@ -1,8 +1,14 @@
+import firebaseConfig from './models/firebaseConfig.js';
 import tasksService from './service/tasksService.js';
+import apiService from './service/weatherService.js';
+import weatherService from './service/weatherService.js';
+
+let selectedTextContent = 'Today';
+let selectedNum = 0;
 
 document.querySelector("#newTaskStartDTButton").addEventListener("click", () => datepicker("#newTaskStartDT"));
 document.querySelector("#newTaskEndDTButton").addEventListener("click", () => datepicker("#newTaskEndDT"));
-document.getElementById('addTaskForm').addEventListener('submit', () => {
+document.getElementById('taskForm').addEventListener('submit', () => {
     addTasks();
     resetFormFields();
 });
@@ -17,13 +23,13 @@ const datepicker = (id) => {
 }
 
 const resetFormFields = () => {
-    const form = document.getElementById('addTaskForm');
+    const form = document.getElementById('taskForm');
     const inputs = form.elements;
 
     // Reset the form fields to their initial state
     for (let i = 0; i < inputs.length; i++) {
-        // submit value should not be cleared
-        if (inputs[i].type === 'submit') {
+        // submit and radio buttons value should not be reset
+        if (inputs[i].type === 'submit' || inputs[i].type === 'radio') {
             inputs[i].disabled = false;
             continue;
         }
@@ -44,13 +50,29 @@ let items = [];
 
 const priorityOptions = {
     'unassigned': '#f5f5f5',
-    'low': '#82D665',
-    'medium': '#E5B772',
-    'high': '#E99893',
+    'low': '#A4E8B3',
+    'medium': '#E8C9A4',
+    'high': '#E8ACA4',
 };
+
+const getCurrentUserId = () => {
+    // return firebase.auth().currentUser.uid;
+    return '1';//for testing purposes
+}
+
+//Helper function
+const createDiv = (className, id) => {
+    const div = document.createElement('div');
+    div.className = className;
+    if (id) div.id = id;
+    return div;
+}
 
 const showTasks = async () => {
     items = await tasksService.getAllTasks();
+
+    const uid = getCurrentUserId();
+    items = items.filter(item => item.userID === uid);
 
     const cardContent = document.getElementById('card-body');
     cardContent.style.height = " 700px";
@@ -66,15 +88,6 @@ const showTasks = async () => {
     `;
         return;
     }
-
-    //Helper function
-    const createDiv = (className, id) => {
-        const div = document.createElement('div');
-        div.className = className;
-        if (id) div.id = id;
-        return div;
-    }
-
     const taskContainer = createDiv('d-flex align-items-center col mx-5 mb-3 my-3 p-3', 'taskContainer');
 
     const headerDiv = createDiv('headerDiv w-100 justify-content-between');
@@ -114,8 +127,6 @@ const showTasks = async () => {
     const hr = document.createElement('hr');
     hr.className = 'double w-100';
     taskContainer.appendChild(hr);
-
-    const dueTaskContainer = createDiv('d-flex align-items-center justify-content-between col mx-5 my-3', 'dueTaskContainer');
 
     const scrollContainer = createDiv('w-100 m-3 overflow-auto pe-3', 'scrollContainer');
     scrollContainer.style.height = '450px';
@@ -161,6 +172,8 @@ const showTasks = async () => {
                     itemBox.removeChild(colorCard);
                     item.priority = priority;
                     tasksService.updateTask(item);
+
+                    showTasks();
                 };
                 colorCardBody.appendChild(button);
             }
@@ -190,9 +203,230 @@ const showTasks = async () => {
 
     taskContainer.appendChild(clearButton);
     cardContent.appendChild(taskContainer);
-    cardContent.appendChild(dueTaskContainer);
+
+    //create div for duetaskcontainer fitting the width of the taskDetectionContainer
+    const taskDetectionContainer = createDiv('d-flex align-items-center  col mx-5 my-3', 'dueTaskContainer');
+    //Header
+    const dueTaskHeader = document.createElement('h2');
+    dueTaskHeader.textContent = 'Upcoming Tasks';
+    dueTaskHeader.style.fontFamily = 'Roboto Mono';
+    dueTaskHeader.style.fontStyle = 'normal';
+    dueTaskHeader.style.color = '#676767';
+    dueTaskHeader.classList.add('ms-3', 'mt-3');
+
+    taskDetectionContainer.appendChild(dueTaskHeader);
+
+    //Due tasks container
+    const dueTaskSelectionContainer = createDiv('w-100 d-flex align-items-center justify-content-between', 'dueTaskSelectionContainer');
+
+    //Sub header
+    const dueTaskHeader2 = document.createElement('h3');
+    dueTaskHeader2.textContent = 'Due ';
+    dueTaskHeader2.style.fontFamily = 'Roboto Mono';
+    dueTaskHeader2.style.fontStyle = 'normal';
+    dueTaskHeader2.style.color = '#676767';
+    dueTaskHeader2.classList.add('ms-3', 'd-flex');
+
+    //Dropdown
+    const dueTaskHeaderDropdown = document.createElement('select');
+    dueTaskHeaderDropdown.classList.add('form-select', 'w-100', 'ms-3', 'me-3');
+
+    const dueTaskHeaderPlaceholderOption = document.createElement('option');
+    dueTaskHeaderPlaceholderOption.textContent = selectedTextContent;
+    dueTaskHeaderPlaceholderOption.value = selectedNum;
+    dueTaskHeaderPlaceholderOption.hidden = true;
+
+    dueTaskHeaderDropdown.appendChild(dueTaskHeaderPlaceholderOption);
+
+    const options = [
+        { text: 'Overdue', value: -1 },
+        { text: 'Today', value: 0 },
+        { text: 'Tomorrow', value: 1 },
+        { text: 'This week', value: 7 },
+        { text: 'Next week', value: 14 }
+    ];
+
+    options.forEach(option => {
+        const optionElement = document.createElement('option');
+        optionElement.textContent = option.text;
+        optionElement.value = option.value;
+        dueTaskHeaderDropdown.appendChild(optionElement);
+    });
+
+    dueTaskHeaderDropdown.style.fontFamily = 'Roboto Mono';
+    dueTaskHeaderDropdown.style.fontStyle = 'normal';
+    dueTaskHeaderDropdown.style.color = '#676767';
+
+    dueTaskHeader2.appendChild(dueTaskHeaderDropdown);
+    taskDetectionContainer.appendChild(dueTaskHeader2);
+
+    //scroll container for due tasks
+    const dueTaskScrollContainer = createDiv('w-100 overflow-auto', 'dueTaskScrollContainer');
+    dueTaskScrollContainer.style.height = '180px';
+
+    dueTaskHeaderDropdown.addEventListener('change', (e) => {
+        dueTaskScrollContainer.innerHTML = '';
+        selectedTextContent = e.target.selectedOptions[0].textContent;
+        selectedNum = e.target.value;
+        showDueTasks(selectedNum);
+    });
+
+    taskDetectionContainer.appendChild(dueTaskScrollContainer);
+
+    // Weather content container
+    const weatherContentContainer = document.createElement('div');
+    weatherContentContainer.classList.add('d-flex', 'align-items-center', 'justify-content-center');
+
+    const currentWeatherContainer = await showWeather();
+    weatherContentContainer.appendChild(currentWeatherContainer);
+
+    const weatherImpactedTasksHeader = document.createElement('text');
+    weatherImpactedTasksHeader.textContent = 'Weather Impacted Task(s)';
+    weatherImpactedTasksHeader.style.fontFamily = 'Roboto Mono';
+    weatherImpactedTasksHeader.style.fontStyle = 'normal';
+
+    //Weather Scroll Container
+    const weatherImpactedTaskScrollContainer = createDiv('w-100 overflow-auto', 'weatherImpactedTaskScrollContainer');
+    weatherImpactedTaskScrollContainer.style.height = '180px';
+
+    taskDetectionContainer.appendChild(hr);
+    taskDetectionContainer.appendChild(weatherContentContainer);
+    taskDetectionContainer.appendChild(weatherImpactedTasksHeader);
+
+    taskDetectionContainer.appendChild(weatherImpactedTaskScrollContainer);
+
+    cardContent.appendChild(taskDetectionContainer);
+    showDueTasks(selectedNum);
+    showWeatherImpactedTasks(items);
 }
 
+const showWeather = async () => {
+    const weatherData = await apiService.getWeatherData();
+    const weatherCondition = weatherData.weather[0].main.toLowerCase();
+    const weatherConditions = ['light rain', 'moderate rain', 'rain', 'shower rain', 'thunderstorm'];
+    const isWeatherImpacted = weatherConditions.some(condition => weatherCondition.includes(condition));
+
+    // Create weather container
+    const currentWeatherContainer = createDiv('d-flex flex-column align-items-center justify-content-center');
+
+    const weatherTextHeader = document.createElement('h2');
+    weatherTextHeader.textContent = isWeatherImpacted ? 'Weather Warning!' : 'Weather Forecast';
+    weatherTextHeader.style.fontFamily = 'Roboto Mono';
+    weatherTextHeader.style.fontStyle = 'normal';
+    weatherTextHeader.style.color = '#676767';
+    weatherTextHeader.style.marginBottom = '0px';
+    
+    currentWeatherContainer.appendChild(weatherTextHeader);
+
+    const weathercontent = createDiv('d-flex align-items-center justify-content-center', 'weathercontent');
+
+    const weatherText = document.createElement('h3');
+    weatherText.textContent = weatherData.weather[0].description;
+    weatherText.style.fontFamily = 'Roboto Mono';
+    weatherText.style.fontStyle = 'normal';
+    weatherText.style.color = '#676767';
+    weatherText.style.marginTop = '0px';
+    weathercontent.appendChild(weatherText);
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`;
+    weatherIcon.alt = 'Weather Icon';
+    weathercontent.appendChild(weatherIcon);
+
+    currentWeatherContainer.appendChild(weathercontent);
+
+    return currentWeatherContainer;
+};
+
+const showWeatherImpactedTasks = async (item) => {
+    const weatherImpactedTaskScrollContainer = document.getElementById('weatherImpactedTaskScrollContainer');
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0); //to compare only the date
+
+    //only impact outdoor tasks
+    const outdoorTasks = item.filter(task => task.inOrOut === 'outdoor');
+
+    //get weather data
+    const weatherData = await apiService.getWeatherData();
+
+    //check for weather impacted tasks
+
+    outdoorTasks.forEach((task) => {
+        const taskDate = new Date(task.endDT);
+        taskDate.setHours(0, 0, 0, 0);
+
+        const dueinDays = Math.floor((taskDate - currentDate) / (1000 * 60 * 60 * 24));
+
+        const weatherCondition = weatherData.weather[0].main.toLowerCase();
+        const weatherConditions = ['light rain', 'rain', 'shower rain', 'thunderstorm'];
+        const isWeatherImpacted = weatherConditions.some(condition => weatherCondition.includes(condition));
+
+        if (dueinDays == 0 && isWeatherImpacted) {
+            const weather = weatherData.weather[0].description;
+            const weatherIcon = weatherData.weather[0].icon;
+
+            const weatherImpactedTaskElement = createDiv('w-100 d-flex');
+            const weatherImpactedTaskBox = createDiv('rounded w-100 p-2 mx-3 mt-3 position-relative');
+            weatherImpactedTaskBox.textContent = task.title;
+            weatherImpactedTaskBox.style.fontStyle = 'normal';
+            weatherImpactedTaskBox.style.color = '#676767';
+            weatherImpactedTaskBox.style.wordBreak = 'break-word';
+            weatherImpactedTaskBox.style.backgroundColor = priorityOptions[task.priority] || '#f5f5f5';
+            weatherImpactedTaskElement.appendChild(weatherImpactedTaskBox);
+            weatherImpactedTaskScrollContainer.appendChild(weatherImpactedTaskElement);
+        }
+    });
+
+    if (weatherImpactedTaskScrollContainer.innerHTML === '') {
+
+        //change weatherimpactedtaskheader content to "No weather impacted tasks"
+        const weatherImpactedTasksHeader = document.querySelector('text');
+        weatherImpactedTasksHeader.textContent = 'No Weather Impacted Task(s)';
+        
+    }
+
+}
+
+const showDueTasks = async (selectedNum) => {
+    const currentDate = new Date();
+    currentDate.setHours(0, 0, 0, 0); //to compare only the date
+
+    const dueTaskScrollContainer = document.getElementById('dueTaskScrollContainer');
+
+    items.sort((a, b) => new Date(a.endDT) - new Date(b.endDT));
+
+    items.forEach((item) => {
+        const dueDate = new Date(item.endDT);
+        dueDate.setHours(0, 0, 0, 0);
+
+        let dueinDays = Math.floor((dueDate - currentDate) / (1000 * 60 * 60 * 24));
+        if (dueinDays < 0) {
+            dueinDays = -1;
+        }
+
+        // Show tasks that are due today, tomorrow, this week, next week, or overdue
+        if ((dueinDays == selectedNum) ||
+            (selectedNum == 7 && dueinDays >= 0 && dueinDays < selectedNum) ||
+            (selectedNum == 14 && dueinDays >= 7 && dueinDays < selectedNum)) {
+
+            const dueitemElement = createDiv('w-100 d-flex');
+            const dueitemBox = createDiv('rounded w-100 p-2 mx-3 mt-3 position-relative');
+            dueitemBox.textContent = item.title;
+            dueitemBox.style.fontStyle = 'normal';
+            dueitemBox.style.color = '#676767';
+            dueitemBox.style.wordBreak = 'break-word';
+            dueitemBox.style.backgroundColor = priorityOptions[item.priority] || '#f5f5f5';
+            dueitemElement.appendChild(dueitemBox);
+            dueTaskScrollContainer.appendChild(dueitemElement);
+        }
+    });
+
+    if (dueTaskScrollContainer.innerHTML === '') {
+        const noDueTasks = createDiv('w-100 d-flex justify-content-center');
+        noDueTasks.textContent = 'No due task(s)';
+        dueTaskScrollContainer.appendChild(noDueTasks);
+    }
+}
 
 const removeColorCard = (e) => {
     const targetDocument = e.target;
@@ -205,30 +439,58 @@ const removeColorCard = (e) => {
 const viewDetailedTasks = (task) => {
     $('#staticBackdrop').modal('show');
 
-    const form = document.getElementById('addTaskForm');
+    const form = document.getElementById('taskForm');
+    const header = document.getElementById('newTaskLabel');
+    header.textContent = 'Task Details';
+
     const inputs = form.elements;
 
     for (let i = 0; i < inputs.length; i++) {
 
         if (inputs[i].type === 'text') {
             inputs[i].style.backgroundColor = '#D7D7D7';
-        }
-        if (task[inputs[i].name]) {
             inputs[i].value = task[inputs[i].name];
+        } else if (inputs[i].name === 'inOrOut') {
+            inputs[i].checked = task[inputs[i].name] === inputs[i].value;
         }
+
         inputs[i].disabled = true;
     }
 
+    //Create Edit Button
     const modalFooter = $('.modal-footer');
     modalFooter.empty();
     modalFooter.append('<button id="editButton" type="button" class="btn btn-confirm">Edit</button>');
 
     $('#editButton').on('click', () => editTaskView(task));
-    $('#staticBackdrop').on('hidden.bs.modal', resetFormFields);
+    $('#staticBackdrop').on('hidden.bs.modal', () => {
+        resetFormFields();
+
+        const modalFooter = $('.modal-footer');
+        modalFooter.empty();
+        modalFooter.append('<button id="confirmButton" type="submit" class="btn btn-confirm" data-bs-dismiss="modal">Confirm</button>');
+        modalFooter.append('<button id="cancelButton" type="button" class="btn btn-cancel" data-bs-dismiss="modal" >Cancel</button>');
+
+        for (let i = 0; i < inputs.length; i++) {
+
+            if (inputs[i].type === 'text') {
+                inputs[i].style.backgroundColor = '#F6F4F3';
+            }
+
+            if (inputs[i].type !== 'confirmButton' && inputs[i].type !== 'cancelButton') {
+                inputs[i].disabled = false;
+            }
+        }
+    });
 }
 
 const addTasks = () => {
-    const form = document.getElementById('addTaskForm');
+
+    const uid = getCurrentUserId();
+    const header = document.getElementById('newTaskLabel');
+    header.textContent = 'New Task';
+
+    const form = document.getElementById('taskForm');
     const interestedInputs = [];
     interestedInputs.push(...form.querySelectorAll('input[type="text"]'));
     interestedInputs.push(...form.querySelectorAll('input[name="inOrOut"]:checked'));
@@ -239,19 +501,27 @@ const addTasks = () => {
         taskObj[input.name] = input.value;
     });
 
+    //add userID to taskObj
+    taskObj.userID = uid;
+
+    console.log(taskObj);
     taskObj.priority = 'unassigned';
     tasksService.addTask(taskObj);
+
     showTasks();
 }
 
 const editTaskView = (task) => {
+    const header = document.getElementById('newTaskLabel');
+    header.textContent = 'Edit Task';
+
     const modalFooter = $('.modal-footer');
     modalFooter.empty();
 
     modalFooter.append('<button id="confirmButton" type="button" class="btn btn-confirm">Confirm</button>');
     modalFooter.append('<button id="cancelButton" type="button" class="btn btn-cancel">Cancel</button>');
 
-    const form = document.getElementById('addTaskForm');
+    const form = document.getElementById('taskForm');
     const inputs = form.elements;
 
     for (let i = 0; i < inputs.length; i++) {
@@ -266,18 +536,26 @@ const editTaskView = (task) => {
     }
 
     $('#confirmButton').on('click', () => confirmEditTasks(task));
-    $('#cancelButton').on('click', () => viewDetailedTasks(task));
-
+    $('#cancelButton').on('click', () => {
+        viewDetailedTasks(task);
+    });
 }
 
 const confirmEditTasks = async (task) => {
-    const form = document.getElementById('addTaskForm');
+    const form = document.getElementById('taskForm');
     const input = form.elements;
 
     for (let i = 0; i < input.length; i++) {
         if (input[i].type === 'submit' || input[i].type === 'button') continue;
-        task[input[i].name] = input[i].value;
+
+        if (input[i].type === 'text') {
+            task[input[i].name] = input[i].value;
+        } else if (input[i].name === 'inOrOut' && input[i].checked) {
+            task[input[i].name] = input[i].value;
+        }
     }
+
+    console.log(task);
 
     await tasksService.updateTask(task);
     viewDetailedTasks(task);
@@ -294,5 +572,7 @@ const deleteTasks = async () => {
 
     showTasks();
 };
+
+window.resetFormFields = resetFormFields;
 
 showTasks();
