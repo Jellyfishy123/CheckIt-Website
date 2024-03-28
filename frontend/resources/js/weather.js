6bb0d7d6f739d325c99cd4e02a692daa7f0b2000
jhmarkkk <93315900+jhmarkkk@users.noreply.github.com>
2024-03-28 16:51:41 +0800

    added homepage checkit href

--- frontend/resources/js/weather.js
+++ frontend/resources/js/weather.js
@@ -5,13 +5,13 @@ const showWeather = async () => {
     const weatherCondition = weatherData.weather[0].main.toLowerCase();
     const weatherConditions = ['light rain', 'moderate rain', 'rain', 'shower rain', 'thunderstorm'];
     const isWeatherImpacted = weatherConditions.some(condition => weatherCondition.includes(condition));
+    console.log(weatherCondition);
     
     var imageShown = isWeatherImpacted ? "resources/images/tomorrow_rain.png" : "resources/images/tomorrow_sun.png";
     return imageShown;
 }
 
 const currentWeather = await showWeather();
-console.log(currentWeather);
 
 var weatherImg = document.getElementById('weatherImg');
 weatherImg.src = currentWeather;
