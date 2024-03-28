ce8b164be1958e0b81d80a579fd54a94bfb6c774
jhmarkkk <93315900+jhmarkkk@users.noreply.github.com>
2024-03-28 16:41:05 +0800

    added weather func to homepage

---
+++ frontend/resources/js/service/weatherService.js
@@ -0,0 +1,19 @@
+import { weatherAPIConfig } from '../models/weatherConfig.js';
+
+const getWeatherData = async () => {
+    const apiKey = weatherAPIConfig.apiKey;
+    const city = 'Singapore'; //testing with countries that are currently raining
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
+
+export default {
+    getWeatherData
+}
