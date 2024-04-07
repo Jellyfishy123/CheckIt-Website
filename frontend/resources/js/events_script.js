93e6bf45f7642b4c80c6c18204c6566951822ed1
OofMani <theadityamani@gmail.com>
2024-04-07 12:32:12 +0800

    events page completed

--- frontend/resources/js/events_script.js
+++ frontend/resources/js/events_script.js
@@ -1,73 +1,81 @@
-const { getJson } = require("serpapi");
+//const { getJson } = require("serpapi");
 
 
 const api_key = "301f554c52f39fbdc0232a29b2f336b059e18c684ca5bb8084a6f1d80573eed0"
 
-const formEl = document.querySelector("form")
-const inputEl1 = document.getElementById("EventSearch")
-const inputEl2 = document.getElementById("LocSearch")
-const inputEl3 = document.getElementById("dateSearch")
+async function fetchData(){
 
-const searchResults = document.querySelector(".search-results")
-const showMore = document.getElementById("show-more-button")
+  try{
 
-let inputData = ""
-let pageNo = 1;
+      const eventName = document.getElementById("EventSearch").value.toLowerCase();
+      eventLoc = document.getElementById("LocSearch").value.toLowerCase();
+      if(!eventLoc == ""){
+        eventLoc = "in " + eventLoc;
+      }
+      console.log(eventName.value);
+      const response = await fetch(`https://serpapi.com/search.json?engine=google_events&q=${eventName}${eventLoc}&api_key=${api_key}`);
+
+      if(!response.ok){
+          throw new Error("Could not fetch resource");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      
+      /*
+      for (let i =0; i <3; i++){
+        const image = data.events_results[i].image;
+        let Id = i.toString();
+        const img_ID = "img"+Id;
+        const imgElement = document.getElementById(img_ID);
+
+        const desc_ID = "desc"+Id;
+        const desc = data.events_results[i].title;
+        const descElement = document.getElementById(desc_ID);
+        imgElement.src = image;
+        descElement1.innerHTML = desc;
+      }
+      
+      */
+      
+      const image1 = data.events_results[0].image;
+      const imgElement = document.getElementById("img1");
+      const desc1 = data.events_results[0].title;
+      const desc1Element = document.getElementById("desc1");
+      imgElement.src = image1;
+      desc1Element.innerHTML = desc1;
+      const link1 = data.events_results[0].link;
+      desc1Element.href = link1;
+      imgElement.style = "display:block;";
+      desc1Element.style = "display:block;";
+
+      const image2 = data.events_results[1].image;
+      const img2Element = document.getElementById("img2");
+      const desc2 = data.events_results[1].title;
+      const desc2Element = document.getElementById("desc2");
+      img2Element.src = image2;
+      desc2Element.innerHTML = desc2;
+      const link2 = data.events_results[1].link;
+      desc2Element.href = link2;
+      img2Element.style = "display:block;";
+      desc2Element.style = "display:block;";
+
+      const image3 = data.events_results[2].image;
+      const img3Element = document.getElementById("img3");
+      const desc3 = data.events_results[2].title;
+      const desc3Element = document.getElementById("desc3");
+      img3Element.src = image3;
+      desc3Element.innerHTML = desc3;
+      const link3 = data.events_results[2].link;
+      desc3Element.href = link3;
+      img3Element.style = "display:block;";
+      desc3Element.style = "display:block;";
+      
 
 
 
-
-
-async function searchImages() {
-    inputData = inputEl1.value;
-    const url = `https://serpapi.com/search.json?engine=google_events&q=${inputData}&hl=en&api_key=${api_key}`;
-    
-    getJson({
-        engine: "google_events",
-        q: inputEl1.value,
-        hl: "en",
-        //gl: "us",
-        api_key: "301f554c52f39fbdc0232a29b2f336b059e18c684ca5bb8084a6f1d80573eed0"
-      }, (json) => {
-        console.log(json["events_results"]);
-      });
-    const events_results = await fetch(url);
-    const data = await events_results.json();
-  
-    const results = data.events_results;
-  
-    if (page === 1) {
-      searchResults.innerHTML = "";
-    }
-  
-    results.map((result) => {
-      const imageWrapper = document.createElement("div");
-      imageWrapper.classList.add("search-result");
-      const image = document.createElement("img");
-      image.src = result.thumbnail;
-      image.alt = "";
-      const imageLink = document.createElement("a");
-      imageLink.href = result.link;
-      imageLink.target = "_blank";
-      imageLink.textContent = result.title;
-  
-      imageWrapper.appendChild(image);
-      imageWrapper.appendChild(imageLink);
-      searchResults.appendChild(imageWrapper);
-    });
-  
-    page++;
-    if (page > 1) {
-      showMore.style.display = "block";
-    }
   }
-  
-  formEl.addEventListener("submit", (event) => {
-    event.preventDefault();
-    //page = 1;
-    searchImages();
-  });
-  
-  showMore.addEventListener("click", () => {
-    searchImages();
-  });
+  catch(error){
+      console.error(error);
+  }
+}
